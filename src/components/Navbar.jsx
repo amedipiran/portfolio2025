7e776311ae2c7c12b03159ec7921467f928e26e1
Piran <robertopiranamedi@gmail.com>
2025-07-03 19:15:24 +0200

    Added mouseleave on logo and color gradient.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,35 +11,14 @@ const Navbar = () => {
   const toggleMenu = () => setMenuOpen(prev => !prev);
 
   useEffect(() => {
-    const handleScroll = () => {
-      setScrolled(window.scrollY > window.innerHeight * 0.2);
-    };
-
-    const handleResize = () => {
-      setSmallWindow(window.innerWidth < 1000);
-    };
-
-    handleScroll();
-    handleResize();
-
-    window.addEventListener('scroll', handleScroll);
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let interval = null;
 
     const handleHover = (e) => {
-      let iterations = 0;
-      const originalText = e.target.dataset.value;
-
       clearInterval(interval);
+      const originalText = e.target.dataset.value;
+      let iterations = 0;
+
       interval = setInterval(() => {
         e.target.innerText = e.target.innerText
             .split("")
@@ -57,17 +36,44 @@ const Navbar = () => {
           clearInterval(interval);
           e.target.innerText = originalText;
         }
-      }, 30);
+      }, 50);
+    };
+
+    const handleLeave = (e) => {
+      clearInterval(interval);
+      const originalText = e.target.dataset.value;
+      let iterations = originalText.length;
+
+      interval = setInterval(() => {
+        e.target.innerText = originalText
+            .split("")
+            .map((letter, index) => {
+              if (index >= iterations) {
+                return originalText[index];
+              }
+              return letters[Math.floor(Math.random() * 26)];
+            })
+            .join("");
+
+        iterations -= 1 / 2;
+
+        if (iterations <= 0) {
+          clearInterval(interval);
+          e.target.innerText = originalText;
+        }
+      }, 50);
     };
 
     const logo = logoRef.current;
     if (logo) {
-      logo.addEventListener('mouseover', handleHover);
+      logo.addEventListener("mouseover", handleHover);
+      logo.addEventListener("mouseleave", handleLeave);
     }
 
     return () => {
       if (logo) {
-        logo.removeEventListener('mouseover', handleHover);
+        logo.removeEventListener("mouseover", handleHover);
+        logo.removeEventListener("mouseleave", handleLeave);
       }
       clearInterval(interval);
     };
