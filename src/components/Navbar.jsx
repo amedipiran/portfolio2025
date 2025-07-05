3c0bf7765ce20a41fe606392cddc7cf580dfcbe0
Piran <robertopiranamedi@gmail.com>
2025-07-06 01:31:34 +0200

    fixed the links in navbar to not have delay after initial animation is finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,24 @@ const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [smallWindow, setSmallWindow] = useState(window.innerWidth < 1000);
   const [revealed, setRevealed] = useState(false);
+  const [animationDone, setAnimationDone] = useState(false);
   const logoRef = useRef(null);
 
   const handleLinkClick = () => setMenuOpen(false);
   const toggleMenu = () => setMenuOpen(prev => !prev);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const revealTimer = setTimeout(() => {
       setRevealed(true);
+
+      const cleanupTimer = setTimeout(() => {
+        setAnimationDone(true);
+      }, 1400);
+
+      return () => clearTimeout(cleanupTimer);
     }, 500);
-    return () => clearTimeout(timer);
+
+    return () => clearTimeout(revealTimer);
   }, []);
 
   useEffect(() => {
@@ -97,7 +105,7 @@ const Navbar = () => {
 
   return (
       <div className={`${smallWindow && scrolled ? 'navbar-glass blur' : ''} ${revealed ? 'navbar-revealed' : ''}`}>
-        <div className="navbar-logo opacity link-delay-1">
+        <div className={`navbar-logo opacity ${revealed && !animationDone ? 'link-delay-1' : ''}`}>
           <a href="#index" data-cursor-hover data-value="Piran" className="logo-link" ref={logoRef}>
             Piran
           </a>
@@ -106,21 +114,41 @@ const Navbar = () => {
 
         <header className={`header-absolute ${scrolled ? 'header-fixed' : ''}`}>
           <nav className={`navbar-links ${scrolled ? 'navbar-glass blur' : ''} ${menuOpen ? 'open' : ''}`}>
-            <a href="#hero" className="fade-in-left link-delay-1" data-cursor-link onClick={handleLinkClick}>
+            <a
+                href="#hero"
+                className={`fade-in-left ${revealed && !animationDone ? 'link-delay-1' : ''}`}
+                data-cursor-link
+                onClick={handleLinkClick}
+            >
               <p>01</p> // home
             </a>
-            <a href="#about" className="fade-in-left link-delay-2" data-cursor-link onClick={handleLinkClick}>
+            <a
+                href="#about"
+                className={`fade-in-left ${revealed && !animationDone ? 'link-delay-2' : ''}`}
+                data-cursor-link
+                onClick={handleLinkClick}
+            >
               <p>02</p> // about
             </a>
-            <a href="#" className="fade-in-left link-delay-3" data-cursor-link onClick={handleLinkClick}>
+            <a
+                href="#"
+                className={`fade-in-left ${revealed && !animationDone ? 'link-delay-3' : ''}`}
+                data-cursor-link
+                onClick={handleLinkClick}
+            >
               <p>03</p> // work
             </a>
-            <a href="#" className="fade-in-left link-delay-4" data-cursor-link onClick={handleLinkClick}>
+            <a
+                href="#"
+                className={`fade-in-left ${revealed && !animationDone ? 'link-delay-4' : ''}`}
+                data-cursor-link
+                onClick={handleLinkClick}
+            >
               <p>04</p> // contact
             </a>
           </nav>
 
-          <div className={`hamburger-container fade-in-left link-delay-3`}>
+          <div className={`hamburger-container fade-in-left ${revealed && !animationDone ? 'link-delay-3' : ''}`}>
             <svg
                 className={`ham hamRotate ham7 ${menuOpen ? 'active' : ''}`}
                 viewBox="0 0 100 100"
@@ -131,14 +159,11 @@ const Navbar = () => {
                   className="line top"
                   d="m 70,33 h -40 c 0,0 -6,1.368796 -6,8.5 0,7.131204 6,8.5013 6,8.5013 l 20,-0.0013"
               />
-              <path
-                  className="line middle"
-                  d="m 70,50 h -40"
-              />
+              <path className="line middle" d="m 70,50 h -40" />
               <path
                   className="line bottom"
                   d="m 69.575405,67.073826 h -40 c -5.592752,0 -6.873604,-9.348582 1.371031,-9.348582
-                  8.244634,0 19.053564,21.797129 19.053564,12.274756 l 0,-40"
+                8.244634,0 19.053564,21.797129 19.053564,12.274756 l 0,-40"
               />
             </svg>
           </div>
