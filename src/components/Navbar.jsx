b7223d732806f33acfc5a3b640dee680de2f3d4c
Piran <robertopiranamedi@gmail.com>
2025-07-06 00:43:07 +0200

    change hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,15 +14,12 @@ const Navbar = () => {
     const handleScroll = () => {
       setScrolled(window.scrollY > window.innerHeight * 0.2);
     };
-
     const handleResize = () => {
       setSmallWindow(window.innerWidth < 1000);
     };
-
     handleScroll();
     window.addEventListener('scroll', handleScroll);
     window.addEventListener('resize', handleResize);
-
     return () => {
       window.removeEventListener('scroll', handleScroll);
       window.removeEventListener('resize', handleResize);
@@ -37,7 +34,6 @@ const Navbar = () => {
       clearInterval(interval);
       const originalText = e.target.dataset.value;
       let iterations = 0;
-
       interval = setInterval(() => {
         const newText = originalText
             .split("")
@@ -46,9 +42,7 @@ const Navbar = () => {
               return letters[Math.floor(Math.random() * letters.length)];
             })
             .join("");
-
         e.target.innerText = newText;
-
         iterations += 0.5;
         if (iterations >= originalText.length) {
           clearInterval(interval);
@@ -61,7 +55,6 @@ const Navbar = () => {
       clearInterval(interval);
       const originalText = e.target.dataset.value;
       let iterations = originalText.length;
-
       interval = setInterval(() => {
         const newText = originalText
             .split("")
@@ -70,9 +63,7 @@ const Navbar = () => {
               return letters[Math.floor(Math.random() * letters.length)];
             })
             .join("");
-
         e.target.innerText = newText;
-
         iterations -= 0.5;
         if (iterations <= 0) {
           clearInterval(interval);
@@ -113,15 +104,33 @@ const Navbar = () => {
 
         <header className={`header-absolute ${scrolled ? 'header-fixed' : ''}`}>
           <nav className={`navbar-links ${scrolled ? 'navbar-glass blur' : ''} ${menuOpen ? 'open' : ''}`}>
-            <a href="#index" data-cursor-link data-cursor-hover onClick={handleLinkClick}><p>01</p> // home</a>
-            <a href="#about" data-cursor-link data-cursor-hover onClick={handleLinkClick}><p>02</p> // about</a>
-            <a href="#" data-cursor-link data-cursor-hover onClick={handleLinkClick}><p>03</p> // work</a>
-            <a href="#" data-cursor-link data-cursor-hover onClick={handleLinkClick}><p>04</p> // contact</a>
+            <a href="#index" data-cursor-link onClick={handleLinkClick}><p>01</p> // home</a>
+            <a href="#about" data-cursor-link onClick={handleLinkClick}><p>02</p> // about</a>
+            <a href="#" data-cursor-link onClick={handleLinkClick}><p>03</p> // work</a>
+            <a href="#" data-cursor-link onClick={handleLinkClick}><p>04</p> // contact</a>
           </nav>
-          <div className={`navbar-burger ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-            <span />
-            <span />
-            <span />
+
+          <div className="hamburger-container">
+            <svg
+                className={`ham hamRotate ham7 ${menuOpen ? 'active' : ''}`}
+                viewBox="0 0 100 100"
+                width="50"
+                onClick={toggleMenu}
+            >
+              <path
+                  className="line top"
+                  d="m 70,33 h -40 c 0,0 -6,1.368796 -6,8.5 0,7.131204 6,8.5013 6,8.5013 l 20,-0.0013"
+              />
+              <path
+                  className="line middle"
+                  d="m 70,50 h -40"
+              />
+              <path
+                  className="line bottom"
+                  d="m 69.575405,67.073826 h -40 c -5.592752,0 -6.873604,-9.348582 1.371031,-9.348582
+       8.244634,0 19.053564,21.797129 19.053564,12.274756 l 0,-40"
+              />
+            </svg>
           </div>
         </header>
       </div>
