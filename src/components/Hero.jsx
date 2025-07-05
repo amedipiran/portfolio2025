394619be9c27ce3ddfe2a2c083efe6218963b2ad
Piran <robertopiranamedi@gmail.com>
2025-07-05 12:53:00 +0200

    Removed unused import

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Spline from '@splinetool/react-spline';
 import '../css/Hero.css';
-import DotLottie from './DotLottie'; 
-import { Player } from '@lottiefiles/react-lottie-player';
+import DotLottie from './DotLottie';
 
 const Hero = () => {
   return (
-<section className="hero" id="index">
+<section className="hero gradient-bg" id="index">
   <div className="text">
     <h1 className='blur'>ROBERTO PIRAN AMEDI</h1>
     <h2 className='blur'>Software Engineer</h2>
